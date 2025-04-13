207e71d4b34ccd5a488e0492501adef493e38c82
Bruno Frade <brunfradrib@gmail.com>
2025-04-13 14:40:35 -0300

    Setup shadcn/ui

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+  import { Button } from './components/ui/button';
+</script>
+
 <template>
-  <h1 class="text-center text-4xl">Hello Vue.js</h1>
+  <h1 class="text-center text-3xl">Hello Vue.js</h1>
 </template>

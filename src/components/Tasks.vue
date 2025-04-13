56a045d073eb12d04846cfa79ba4e0fd9195feeb
Bruno Frade <brunfradrib@gmail.com>
2025-04-13 15:16:42 -0300

    Create the TaskTable component

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
     import AddTask from '@/components/Add-Task.vue';
+    import TasksTable from '@/components/TasksTable.vue';
 </script>
 
 <template>
-    <main class="flex flex-col items-center justify-center h-[80vh]">
+    <main class="flex flex-col items-center justify-center h-[80vh] gap-[3vh] lg:gap-[3vw]">
+        <TasksTable />
         <AddTask />
     </main>
 </template>
---
+++ src/components/TasksTable.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 className="text-neutral-700 text-center text-5xl font-bold">
+        No More Tasks 😄
+    </h1>
+</template>

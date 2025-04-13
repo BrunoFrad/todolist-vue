96a7f29247bf1f9f1e87d42ba42c7ce67e3617fb
Bruno Frade <brunfradrib@gmail.com>
2025-04-13 14:53:11 -0300

    Create the Header component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-  import { Button } from './components/ui/button';
+  import Header from '@/components/Header.vue';
 </script>
 
 <template>
-  <h1 class="text-center text-3xl">Hello Vue.js</h1>
+  <Header></Header>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { Button } from '@/components/ui/button';
+</script>
+
+<template>
+    <header class="flex h-18 items-center justify-between shadow-sm">
+        <h1 class="m-12 text-xl font-light">
+            TodoList
+        </h1>
+        <Button class="m-12 cursor-pointer">
+            <a href="https://github.com/BrunoFrad/todolist-vue" target="_blank">
+                Github
+            </a>
+        </Button>
+    </header>
+</template>

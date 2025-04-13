62bcd9615c1a38ff26512158cb5737f524bfdea8
Bruno Frade <brunfradrib@gmail.com>
2025-04-13 15:48:15 -0300

    Add conditional rendering to TaskTable component

--- src/components/TasksTable.vue
+++ src/components/TasksTable.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+    import { onMounted, ref } from 'vue';
+    
+    const tasksStored = ref<string | null>(null);
+
+    onMounted(() => {
+        tasksStored.value = localStorage.getItem("tasks");
+    });
+
+</script>
+
 <template>
-    <h1 className="text-neutral-700 text-center text-5xl font-bold">
+
+    <h1 v-if="tasksStored == null" className="text-neutral-700 text-center text-5xl font-bold">
         No More Tasks 😄
     </h1>
+
+    <h1 v-else>Tasks...</h1>
+
 </template>
